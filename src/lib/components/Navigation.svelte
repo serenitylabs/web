08c2fc9f4d236fdde2a16de85537c0f704369e19
~ <~>
2023-05-10 23:47:15 +0200

    feat: close sidebar when navigating

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,8 +1,32 @@
+<script lang="ts">
+  import { drawerStore } from '@skeletonlabs/skeleton'
+
+  const menuItems = [
+    {
+      label: 'menu item one',
+      url: '/1',
+    },
+    {
+      label: 'menu item two',
+      url: '/2',
+    },
+    {
+      label: 'menu item three',
+      url: '/3',
+    },
+    {
+      label: 'menu item four',
+      url: '/4',
+    },
+  ]
+
+  const closeDrawer = drawerStore.close
+</script>
+
 <nav class="list-nav p-4">
   <ul>
-    <li><a href="/1">menu item one</a></li>
-    <li><a href="/2">menu item two</a></li>
-    <li><a href="/3">menu item three</a></li>
-    <li><a href="/4">menu item four</a></li>
+    {#each menuItems as item}
+      <li><a href={item.url} on:click={() => closeDrawer}>{item.label}</a></li>
+    {/each}
   </ul>
 </nav>
