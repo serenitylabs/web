e099eb19bde6bafdac9f9ed683c2cc88f7487c88
~ <~>
2023-05-15 21:00:50 +0200

    feat: add glass panel

---
+++ src/lib/components/GlassPanel.svelte
@@ -0,0 +1,3 @@
+<div
+  class="h-5/6 w-5/6 bg-gradient-to-br from-white/80 to-white/5 backdrop-blur-sm rounded-3xl border-2 border-white/20"
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,9 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+  import GlassPanel from '$lib/components/GlassPanel.svelte'
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-  <div class="space-y-5">
-    <h1 class="h1">Let's get cracking bones!</h1>
-    <p>Start by exploring:</p>
-    <ul>
-      <li>
-        <code class="code">/src/routes/+layout.svelte</code> - barebones layout,
-        the CSS import order is critical!
-      </li>
-      <li>
-        <code class="code">/src/app.postcss</code> - minimal css to make the page
-        full screen, may not be relevant for your project
-      </li>
-      <li>
-        <code class="code">/src/routes/+page.svelte</code> - this page, you can replace
-        the contents
-      </li>
-    </ul>
-  </div>
+<div
+  class="grid place-items-center grid-cols-1 h-screen w-screen bg-gradient-to-bl from-pink-300 to-purple-400"
+>
+  <GlassPanel />
 </div>
